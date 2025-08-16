f67ad8fdc8869f3b4293b4505e51d5cd3005e7b1
Dagt <da_gt92@hotmail.com>
2025-08-15 18:20:42 -0600

    Add funding polling and cash-and-carry strategy

--- src/tradingbot/strategies/__init__.py
+++ src/tradingbot/strategies/__init__.py
@@ -1,12 +1,14 @@
 from .breakout_atr import BreakoutATR
 from .momentum import Momentum
 from .mean_reversion import MeanReversion
+from .cash_and_carry import CashAndCarry
 
 # Registry of available strategies, useful for CLI/backtests
 STRATEGIES = {
     BreakoutATR.name: BreakoutATR,
     Momentum.name: Momentum,
     MeanReversion.name: MeanReversion,
+    CashAndCarry.name: CashAndCarry,
 }
 
-__all__ = ["BreakoutATR", "Momentum", "MeanReversion", "STRATEGIES"]
+__all__ = ["BreakoutATR", "Momentum", "MeanReversion", "CashAndCarry", "STRATEGIES"]
