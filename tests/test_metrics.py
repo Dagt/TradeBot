983837cb6c8a22c49f582e79b64944498c15c582
Dagt <da_gt92@hotmail.com>
2025-08-18 14:18:48 -0600

    Add order rejection rate alert and metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,4 +1,10 @@
-from tradingbot.utils.metrics import FILL_COUNT, SLIPPAGE, RISK_EVENTS
+from tradingbot.utils.metrics import (
+    FILL_COUNT,
+    SLIPPAGE,
+    RISK_EVENTS,
+    ORDER_SENT,
+    ORDER_REJECTS,
+)
 
 
 def test_fill_slippage_risk_metrics():
@@ -6,6 +12,8 @@ def test_fill_slippage_risk_metrics():
     FILL_COUNT.clear()
     SLIPPAGE.clear()
     RISK_EVENTS.clear()
+    ORDER_SENT._value.set(0)
+    ORDER_REJECTS._value.set(0)
 
     FILL_COUNT.labels(symbol="BTCUSDT", side="buy").inc()
     samples = list(FILL_COUNT.collect())[0].samples
@@ -23,3 +31,13 @@ def test_fill_slippage_risk_metrics():
     samples = list(RISK_EVENTS.collect())[0].samples
     risk_sample = [s for s in samples if s.name == "risk_events_total" and s.labels["event_type"] == "limit_breach"][0]
     assert risk_sample.value == 1.0
+
+    ORDER_SENT.inc()
+    sent_samples = list(ORDER_SENT.collect())[0].samples
+    sent_sample = [s for s in sent_samples if s.name == "order_sent_total"][0]
+    assert sent_sample.value == 1.0
+
+    ORDER_REJECTS.inc()
+    rej_samples = list(ORDER_REJECTS.collect())[0].samples
+    rej_sample = [s for s in rej_samples if s.name == "order_rejects_total"][0]
+    assert rej_sample.value == 1.0
