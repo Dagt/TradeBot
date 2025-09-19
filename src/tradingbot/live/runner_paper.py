6b1cd07fbb4633247a63fff3eea88b09a6a4325f
Dagt <da_gt92@hotmail.com>
2025-09-18 18:40:23 -0600

    Log paper orders when broker returns new status

--- src/tradingbot/live/runner_paper.py
+++ src/tradingbot/live/runner_paper.py
@@ -1060,7 +1060,7 @@ async def run_paper(
                         continue
                     log_order = False
                     order_qty = qty_close
-                    if status in {"open", "filled"}:
+                    if status in {"open", "filled", "new"}:
                         log_order = True
                     elif status == "canceled" and filled_qty > 0:
                         log_order = True
@@ -1223,7 +1223,7 @@ async def run_paper(
                             continue
                         log_order = False
                         order_qty = qty_scale
-                        if status in {"open", "filled"}:
+                        if status in {"open", "filled", "new"}:
                             log_order = True
                         elif status == "canceled" and filled_qty > 0:
                             log_order = True
@@ -1469,7 +1469,7 @@ async def run_paper(
                 continue
             log_order = False
             order_qty = qty
-            if status in {"open", "filled"}:
+            if status in {"open", "filled", "new"}:
                 log_order = True
             elif status == "canceled" and filled_qty > 0:
                 log_order = True
